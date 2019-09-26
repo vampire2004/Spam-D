9225c059d976a68667dc0c340737324f9fef692e
vampire2004 <55844077+vampire2004@users.noreply.github.com>
2019-09-26 19:53:02 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("626592264256225292")
+setInterval(function() {
+channel.send(`cRDITS`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
